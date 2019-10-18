5ae1566bc1215d9d93832f96cd69c2006161589d
315920958@qq.com <315920958@qq.com>
2019-10-18 10:48:12 +0800

    配置相关

--- src/main/java/org/dragonli/service/general/other/OtherApplication.java
+++ src/main/java/org/dragonli/service/general/other/OtherApplication.java
@@ -2,32 +2,25 @@ package org.dragonli.service.general.other;
 
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.dragonli.service.dubbosupport.DubboApplicationBase;
-import org.dragonli.tools.redis.RedisConfigurationGeneral;
-import org.dragonli.tools.redis.redisson.RedisClientBuilder;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@ComponentScan(basePackages = {"com.alpacaframework"})
 @EnableScheduling
 @SpringBootApplication(
 		exclude={DataSourceAutoConfiguration.class},
-		scanBasePackages={"org.dragonli","org.dragonli.tools.redis","org.dragonli.tools.redis.redisson"})
+		scanBasePackages={"org.dragonli"})
 @DubboComponentScan(basePackages = "org.dragonli.service.general.other")
 public class OtherApplication extends DubboApplicationBase {
 
-	public OtherApplication(@Value("${spring.other-server.application.name}") String applicationName,
-			@Value("${spring.common.registry.address}") String registryAddr,
-			@Value("${spring.other-server.protocol.name}") String protocolName,
-			@Value("${spring.other-server.protocol.port}") Integer protocolPort,
-			@Value("${spring.other-server.scan}") String registryId,
-			@Value("${micro-service-port.other-service-port}") int port) {
+	public OtherApplication(@Value("${service.micro-service.other-service.application-name}") String applicationName,
+			@Value("${service.micro-service.common.registry-address}") String registryAddr,
+			@Value("${service.micro-service.other-service.protocol-name}") String protocolName,
+			@Value("${service.micro-service.other-service.protocol-port}") Integer protocolPort,
+			@Value("${service.micro-service.other-service.scan}") String registryId,
+			@Value("${service.micro-service.other-service.http-port}") int port) {
 //
 		super(applicationName, registryAddr, protocolName, protocolPort, registryId, port);
 	}
